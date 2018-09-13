5c8a658a8d20e8260bef800b72f2e62c8928bd43
TheMrMares <bartosz.rak@acaisoft.com>
2018-09-13 17:57:30 +0200

    feature: ESlint, window, document init...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './app/App'
+import registerServiceWorker from './registerServiceWorker'
+import document from './utils/document'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
